2c659d45b8767e61d7ac8cbc01af2f12d59144f8
Ves <vesmaybevesper@gmail.com>
2025-10-13 11:47:59 -0400

    fix neoforge crash

--- src/main/java/dev/vesper/eveningstarlib/mixin/ClientPacketListener.java
+++ src/main/java/dev/vesper/eveningstarlib/mixin/ClientPacketListener.java
@@ -1,21 +1,19 @@
 package dev.vesper.eveningstarlib.mixin;
 
-//? fabric {
+
 import com.llamalad7.mixinextras.sugar.Local;
-import dev.vesper.eveningstarlib.ModTemplate;
+//? fabric {
 import dev.vesper.eveningstarlib.fabric.events.ClientRespawnEventCallback;
+//?}
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.multiplayer.ClientCommonPacketListenerImpl;
 import net.minecraft.client.multiplayer.CommonListenerCookie;
 import net.minecraft.client.player.LocalPlayer;
 import net.minecraft.network.Connection;
-import net.minecraft.server.MinecraftServer;
 import net.minecraft.world.entity.Entity;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyArg;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(net.minecraft.client.multiplayer.ClientPacketListener.class)
 public abstract class ClientPacketListener extends ClientCommonPacketListenerImpl {
@@ -23,12 +21,13 @@ public abstract class ClientPacketListener extends ClientCommonPacketListenerImp
     protected ClientPacketListener(Minecraft minecraft, Connection connection, CommonListenerCookie commonListenerCookie) {
         super(minecraft, connection, commonListenerCookie);
     }
-
+    //? fabric {
     @ModifyArg(method = "handleRespawn", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/multiplayer/ClientLevel;addEntity(Lnet/minecraft/world/entity/Entity;)V"))
     private Entity onClientPlayerRespawn(Entity entity, @Local(ordinal = 0) LocalPlayer localPlayer){
         LocalPlayer player = (LocalPlayer) entity;
         ClientRespawnEventCallback.EVENT.invoker().onRespawn(this.minecraft.gameMode, localPlayer, player, this.connection);
         return entity;
     }
+    //?}
 }
-//?}
+
