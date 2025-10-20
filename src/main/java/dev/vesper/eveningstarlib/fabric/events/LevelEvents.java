7c1a5450b4d68d96b32224f3646a41252bd33a81
Ves <vesmaybevesper@gmail.com>
2025-10-19 21:28:04 -0400

    Create LOAD & UNLOAD events

---
+++ src/main/java/dev/vesper/eveningstarlib/fabric/events/LevelEvents.java
@@ -0,0 +1,56 @@
+package dev.vesper.eveningstarlib.fabric.events;
+//? fabric {
+import net.fabricmc.fabric.api.event.Event;
+import net.fabricmc.fabric.api.event.EventFactory;
+import net.minecraft.world.level.LevelAccessor;
+
+public abstract class LevelEvents {
+    private final LevelAccessor level;
+
+    private LevelEvents(LevelAccessor level){
+        this.level = level;
+    }
+
+    public LevelAccessor getLevel() {
+        return level;
+    }
+
+    public static class Load extends LevelEvents{
+        public static final Event<Callback> LOAD = EventFactory.createArrayBacked(Callback.class, callbacks -> event -> {
+           for (Callback callback : callbacks)
+               callback.onLoad(event);
+        });
+
+        public Load(LevelAccessor level) {
+            super(level);
+        }
+
+        public void sendEvent(){
+            LOAD.invoker().onLoad(this);
+        }
+
+        public interface Callback{
+            void onLoad(Load event);
+        }
+    }
+
+    public static class Unload extends LevelEvents{
+        public static final Event<Callback> UNLOAD = EventFactory.createArrayBacked(Callback.class, callbacks -> event -> {
+            for (Callback callback : callbacks)
+                callback.onUnload(event);
+        });
+
+        public Unload(LevelAccessor level) {
+            super(level);
+        }
+
+        public void sendEvent() {
+            UNLOAD.invoker().onUnload(this);
+        }
+
+        public interface Callback{
+            void onUnload(Unload event);
+        }
+    }
+}
+//? }
---
+++ src/main/java/dev/vesper/eveningstarlib/mixin/MinecraftMixin.java
@@ -0,0 +1,39 @@
+package dev.vesper.eveningstarlib.mixin;
+//? fabric {
+import dev.vesper.eveningstarlib.fabric.events.LevelEvents;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.screens.ReceivingLevelScreen;
+import net.minecraft.client.gui.screens.Screen;
+import net.minecraft.client.multiplayer.ClientLevel;
+import org.jetbrains.annotations.Nullable;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+//? }
+@Mixin(Minecraft.class)
+public class MinecraftMixin {
+    //? fabric {
+    @Shadow
+    @Nullable
+    public ClientLevel level;
+
+    @Inject(method = "setLevel", at = @At("HEAD"))
+    private void onUnload(ClientLevel clientLevel, ReceivingLevelScreen.Reason reason, CallbackInfo ci){
+        if (this.level != null) new LevelEvents.Unload(this.level).sendEvent();
+    }
+
+    @Inject(method = "disconnect(Lnet/minecraft/client/gui/screens/Screen;Z)V", at = @At(
+            value = "FIELD",
+            target = "Lnet/minecraft/client/Minecraft;level:Lnet/minecraft/client/multiplayer/ClientLevel;",
+            ordinal = 0,
+            shift = At.Shift.AFTER
+    ))
+    private void onDisconnect(Screen screen, boolean bl, CallbackInfo ci) {
+        if (this.level != null) {
+            new LevelEvents.Unload(this.level).sendEvent();
+        }
+    }
+    //? }
+}
