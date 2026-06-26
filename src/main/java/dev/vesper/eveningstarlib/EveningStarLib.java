498fd7c90ed93004ed44cad9c4cea102b0721eac
VesMaybeVesper <vesmaybevesper@gmail.com>
2026-06-26 17:53:33 -0400

    be a bit more organized

--- src/main/java/dev/vesper/eveningstarlib/EveningStarLib.java
+++ src/main/java/dev/vesper/eveningstarlib/EveningStarLib.java
@@ -1,5 +1,11 @@
 package dev.vesper.eveningstarlib;
 
+//? fabric{
+import net.fabricmc.loader.api.FabricLoader;
+//?} neoforge {
+/*import net.neoforged.fml.ModList;
+import net.neoforged.fml.loading.FMLEnvironment;
+*///?}
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -10,5 +16,22 @@ public class EveningStarLib {
 
     public static void init() {
         LOG.info("Initializing {} on {}", MOD_ID, Platform.INSTANCE.loader());
+    }
+
+    public static boolean isDevEnv(){
+        //? fabric{
+        return FabricLoader.getInstance().isDevelopmentEnvironment();
+        //?} neoforge{
+        /*return !FMLEnvironment.isProduction();
+         *///?}
+    }
+
+    public static boolean isModLoaded(String modId){
+        //? fabric {
+        return FabricLoader.getInstance().isModLoaded(modId);
+        //?}
+        //? neoforge {
+        /*return ModList.get().isLoaded(modId);
+         *///?}
     }
 }
--- src/main/java/dev/vesper/eveningstarlib/common/ESLModChecks.java
+++ src/main/java/dev/vesper/eveningstarlib/common/ESLModChecks.java
@@ -1,26 +1,14 @@
 package dev.vesper.eveningstarlib.common;
 
 //? fabric {
-import net.fabricmc.loader.api.FabricLoader;
-//?}
-//? neoforge {
-/*import dev.vesper.eveningstarlib.EveningStarLib;
-import dev.vesper.eveningstarlib.common.helpers.ESLCodeHelpers;
-import net.neoforged.fml.ModList;
-import net.neoforged.fml.loading.FMLEnvironment;
-import net.neoforged.neoforge.common.NeoForge;
-*///?}
 import net.irisshaders.iris.api.v0.IrisApi;
+//?}
+import dev.vesper.eveningstarlib.EveningStarLib;
 
 public class ESLModChecks {
 
     public static boolean isIris(){
-        //? fabric {
-        return FabricLoader.getInstance().isModLoaded("iris");
-        //?}
-        //? neoforge {
-        /*return ModList.get().isLoaded("iris");
-        *///?}
+        return EveningStarLib.isModLoaded("iris");
     }
 
     public static boolean isShaders() {
@@ -31,20 +19,13 @@ public class ESLModChecks {
             return false;
         }
         //?} neoforge && >=26.2{
-        /*if (ESLCodeHelpers.isDevEnv()){
-            EveningStarLib.LOG.warn("IsShaders() currently always returns false on NeoForge due to Iris not supporting 26.2 Neo at time of release. If this is no longer the case please either check for an ESL update or (kindly) yell at me to update it! Thanks!");
-        }
+        /*if (EveningStarLib.isDevEnv()) EveningStarLib.LOG.warn("IsShaders() currently always returns false on NeoForge due to Iris not supporting 26.2 Neo at time of release. If this is no longer the case please either check for an ESL update or (kindly) yell at me to update it! Thanks!");
         return false;
         *///?}
     }
 
 
     public static boolean isSodium(){
-        //? fabric {
-        return FabricLoader.getInstance().isModLoaded("sodium");
-        //?}
-        //? neoforge {
-        /*return ModList.get().isLoaded("sodium");
-        *///?}
+       return EveningStarLib.isModLoaded("sodium");
     }
 }
